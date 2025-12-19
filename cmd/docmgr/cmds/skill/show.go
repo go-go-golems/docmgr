c1a4071c7c509afcd450895184ad8abc64798288
Manuel Odendahl <wesen@ruinwesen.com>
2025-12-19 13:11:11 -0500

    Add Cobra command group for skill commands

---
+++ cmd/docmgr/cmds/skill/show.go
@@ -0,0 +1,25 @@
+package skill
+
+import (
+	"github.com/go-go-golems/docmgr/pkg/commands"
+	"github.com/go-go-golems/docmgr/cmd/docmgr/cmds/common"
+	"github.com/go-go-golems/glazed/pkg/cli"
+	"github.com/spf13/cobra"
+)
+
+func newShowCommand() (*cobra.Command, error) {
+	cmd, err := commands.NewSkillShowCommand()
+	if err != nil {
+		return nil, err
+	}
+	cobraCmd, err := common.BuildCommand(
+		cmd,
+		cli.WithDualMode(true),
+		cli.WithGlazeToggleFlag("with-glaze-output"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return cobraCmd, nil
+}
+
